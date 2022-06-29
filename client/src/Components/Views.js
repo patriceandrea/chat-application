82e1bc63d9c914c595b2b490aec0543f61de54cd
patriceandrea <andrea_pavico98@yahoo.ca>
2022-06-28 20:58:24 -0400

    add login and signup component

--- client/src/Components/Views.js
+++ client/src/Components/Views.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
+import Login from './Login/Login';
+import SignUp from './Login/SignUp';
 
 export const Views = () => {
   return (
-    <Routes><Route path='/' /></Routes>
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/register' element={<SignUp />} />
+      <Route path='*' element={<Login />} />
+    </Routes >
   )
 }
 
