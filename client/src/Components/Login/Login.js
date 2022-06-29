b6baf92ddfb3481596190443fa9e116cabf5e053
patriceandrea <andrea_pavico98@yahoo.ca>
2022-06-29 11:26:28 -0400

    adds error messages with formik

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -4,64 +4,72 @@ import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
 const Login = () => {
+
   const formik = useFormik({
     initialValues: { username: "", password: "" },
     validationSchema: Yup.object({
-      username: Yup.string().required("Username Required").min(6, "Username too short").max(28, "Username too long")
+      username: Yup.string()
+        .required("Username Required")
+        .min(6, "Username too short")
+        .max(28, "Username too long"),
+      password: Yup.string()
+        .required("Password required!")
+        .min(6, "Password too short!")
+        .max(28, "Password too long!")
     }),
     onSubmit: (values, actions) => {
       alert(JSON.stringify(values, null, 2));
       actions.resetForm();
     },
   });
-
   return (
-    <div>
-      <VStack
-        as="form"
-        w={{ base: "90%", md: "500px" }}
-        m="auto"
-        justify="center"
-        h="100vh"
-        spacing="1rem"
-        onSubmit={formik.handleSubmit}
-      >
 
-        <Heading>Log In</Heading>
-        <FormControl>
-          <FormLabel fontSize="lg">Username</FormLabel>
-          <Input
-            name='username'
-            placeholder='Enter username'
-            autoComplete='off'
-            size="lg"
-            value={formik.values.username}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          <FormErrorMessage>Invalid Username</FormErrorMessage>
-        </FormControl>
+    <VStack
+      as="form"
+      w={{ base: "90%", md: "500px" }}
+      m="auto"
+      justify="center"
+      h="100vh"
+      spacing="1rem"
+      onSubmit={formik.handleSubmit}
+    >
 
-        <FormControl>
-          <FormLabel fontSize="lg">Password</FormLabel>
-          <Input
-            name='password'
-            placeholder='Enter password'
-            autoComplete='off'
-            size="lg"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          <FormErrorMessage>Invalid Password</FormErrorMessage>
-        </FormControl>
+      <Heading>Log In</Heading>
+      <FormControl isInvalid={formik.errors.username && formik.touched.username}>
+        <FormLabel fontSize="lg">Username</FormLabel>
+        <Input
+          name='username'
+          placeholder='Enter username'
+          autoComplete='off'
+          size="lg"
+          value={formik.values.username}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        <FormErrorMessage>{formik.errors.username}</FormErrorMessage>
+      </FormControl>
 
-        <ButtonGroup pt="1rem">
-          <Button colorScheme="teal" type='submit'>Log In</Button>
-          <Button>Create Account</Button>
-        </ButtonGroup>
-      </VStack>
-    </div >
+      <FormControl isInvalid={formik.errors.password && formik.touched.password}>
+        <FormLabel fontSize="lg">Password</FormLabel>
+        <Input
+          name='password'
+          placeholder='Enter password'
+          autoComplete='off'
+          size="lg"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+      </FormControl>
+
+      <ButtonGroup pt="1rem">
+        <Button colorScheme="teal" type='submit'>Log In</Button>
+        <Button>Create Account</Button>
+      </ButtonGroup>
+    </VStack>
+
   )
 }
 
