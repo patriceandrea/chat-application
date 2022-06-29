8c2a063d3f71a21e95e714d1ab2b54c9c9f2c41b
patriceandrea <andrea_pavico98@yahoo.ca>
2022-06-29 16:12:06 -0400

    server and socket.io works

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { Server } = require("socket.io");
+const helmet = require("helmet");
+//initializer
+const app = express();
+
+const server = require("http").createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: "true",
+  }
+})
+
+app.use(helmet());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("Hi");
+})
+
+io.on("connect", socket => { })
+
+server.listen(4000, () => {
+  console.log("Server listening on port 4000");
+});
