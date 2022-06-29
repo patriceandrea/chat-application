0656b6b682752f01f3ef65991c91ead54adc590e
patriceandrea <andrea_pavico98@yahoo.ca>
2022-06-28 21:52:25 -0400

    style login form

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,23 +1,32 @@
-import { VStack, ButtonGroup, FormControl, FormLabel, Button, FormErrorMessage, Input } from '@chakra-ui/react'
+import { VStack, ButtonGroup, FormControl, FormLabel, Button, FormErrorMessage, Input, Heading } from '@chakra-ui/react'
 import React from 'react'
 
 const Login = () => {
   return (
     <div>
-      <VStack as="form" w={{ base: "90%", md: "500 px" }} m="auto" justify="center" h="100vh">
+      <VStack
+        as="form"
+        w={{ base: "90%", md: "500px" }}
+        m="auto"
+        justify="center"
+        h="100vh"
+        spacing="1rem"
+      >
+
+        <Heading>Log In</Heading>
         <FormControl>
-          <FormLabel>Username</FormLabel>
-          <Input name='username' placeholder='Enter username' autoComplete='off' />
+          <FormLabel fontSize="lg">Username</FormLabel>
+          <Input name='username' placeholder='Enter username' autoComplete='off' size="lg" />
           <FormErrorMessage>Invalid Username</FormErrorMessage>
         </FormControl>
 
         <FormControl>
-          <FormLabel>Password</FormLabel>
-          <Input name='password' placeholder='Enter password' autoComplete='off' />
+          <FormLabel fontSize="lg">Password</FormLabel>
+          <Input name='password' placeholder='Enter password' autoComplete='off' size="lg" />
           <FormErrorMessage>Invalid Password</FormErrorMessage>
         </FormControl>
 
-        <ButtonGroup>
+        <ButtonGroup pt="1rem">
           <Button colorScheme="teal" type='submit'>Log In</Button>
           <Button>Create Account</Button>
         </ButtonGroup>
