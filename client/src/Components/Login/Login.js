f8b82d56e033be49d31f5630a6a0a36ffdd5ad53
patriceandrea <andrea_pavico98@yahoo.ca>
2022-06-28 21:17:23 -0400

    add a form for username and password

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,10 +1,28 @@
+import { VStack, ButtonGroup, FormControl, FormLabel, Button, FormErrorMessage, Input } from '@chakra-ui/react'
 import React from 'react'
 
 const Login = () => {
   return (
     <div>
+      <VStack as="form" w={{ base: "90%", md: "500 px" }} m="auto" justify="center" h="100vh">
+        <FormControl>
+          <FormLabel>Username</FormLabel>
+          <Input name='username' placeholder='Enter username' autoComplete='off' />
+          <FormErrorMessage>Invalid Username</FormErrorMessage>
+        </FormControl>
 
-    </div>
+        <FormControl>
+          <FormLabel>Password</FormLabel>
+          <Input name='password' placeholder='Enter password' autoComplete='off' />
+          <FormErrorMessage>Invalid Password</FormErrorMessage>
+        </FormControl>
+
+        <ButtonGroup>
+          <Button colorScheme="teal" type='submit'>Log In</Button>
+          <Button>Create Account</Button>
+        </ButtonGroup>
+      </VStack>
+    </div >
   )
 }
 
