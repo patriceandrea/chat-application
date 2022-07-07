cc4e3047d597f0604ccaf99440048faa9ef63659
patriceandrea <andrea_pavico98@yahoo.ca>
2022-07-06 22:01:27 -0400

    adds a attemptRegister controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,6 @@
+const pool = require("../db");
+const bcrypt = require("bcrypt");
+
 module.exports.handleLogin = (res, req) => {
   if (req.session.user && req.session.user.username) {
     console.log("logged in")
@@ -5,4 +8,58 @@ module.exports.handleLogin = (res, req) => {
   } else {
     res.json({ loggedIn: false })
   }
+};
+
+module.exports.attempLogin = async (req, res) => {
+  const potentialLogin = await pool.query(
+    "SELECT id, username, passhash FROM users u  WHERE u.username=$1 ",
+    [req.body.username]
+  );
+
+  if (potentialLogin.rowCount > 0) {
+    const isSamePass = await bcrypt.compare(
+      req.body.password,
+      potentialLogin.rows[0].passhash
+    ); if (isSamePass) {
+      //login 
+      req.session.user = {
+        username: req.body.username,
+        id: potentialLogin.rows[0].id
+      };
+      res.json({ loggedIn: true, user: req.body.username })
+    } else {
+      //not good login
+
+      console.log("not good! OHHHH NOOOooooooo");
+      res.json({ loggedIn: false, status: "Wrong username or password" });
+    }
+  } else {
+    console.log("not good!");
+    res.json({ loggedIn: false, status: "Wrong username or password" });
+  }
+
+};
+
+
+module.exports.attempRegister = async (req, res) => {
+
+  const existingUser = await pool.query("SELECT username from users WHERE  username=$1", [req.body.username]
+  );
+  if (existingUser.rowCount === 0) {
+    //register 
+    const hashedPass = await bcrypt.hash(req.body.password, 10);
+    const newUserQuery = await pool.query("INSERT INTO users(username, passhash) values($1,$2) RETURNING username",
+      [req.body.username, hashedPass]
+    );
+    req.session.user = {
+      username: req.body.username,
+      id: newUserQuery.rows[0].id
+    }
+    res.json({ loggedIn: true, username: req.body.username })
+  } else {
+    res.json({ loggedIn: false, status: "Username taken" })
+  };
+
+
 }
+
--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,66 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const validateForm = require("../controllers/validateForm")
-const pool = require("../db");
-const bcrypt = require("bcrypt");
-const { handleLogin } = require("../controllers/authController")
+const {
+  handleLogin,
+  attempLogin,
+  attempRegister
+} = require("../controllers/authController");
 
 //POST - login
 router.route("/login")
   .get(handleLogin)
-  .post(validateForm, async (req, res) => {
-    const potentialLogin = await pool.query(
-      "SELECT id, username, passhash FROM users u  WHERE u.username=$1 ",
-      [req.body.username]
-    );
-
-    if (potentialLogin.rowCount > 0) {
-      const isSamePass = await bcrypt.compare(
-        req.body.password,
-        potentialLogin.rows[0].passhash
-      ); if (isSamePass) {
-        //login 
-        req.session.user = {
-          username: req.body.username,
-          id: potentialLogin.rows[0].id
-        };
-        res.json({ loggedIn: true, user: req.body.username })
-      } else {
-        //not good login
-
-        console.log("not good! OHHHH NOOOooooooo");
-        res.json({ loggedIn: false, status: "Wrong username or password" });
-      }
-    } else {
-      console.log("not good!");
-      res.json({ loggedIn: false, status: "Wrong username or password" });
-    }
-
-
-
-  });
+  .post(validateForm, attempLogin);
 
 //POST - sign up/register
-router.post("/register", validateForm, async (req, res) => {
-
-
-  const existingUser = await pool.query("SELECT username from users WHERE  username=$1", [req.body.username]
-  );
-  if (existingUser.rowCount === 0) {
-    //register 
-    const hashedPass = await bcrypt.hash(req.body.password, 10);
-    const newUserQuery = await pool.query("INSERT INTO users(username, passhash) values($1,$2) RETURNING username",
-      [req.body.username, hashedPass]
-    );
-    req.session.user = {
-      username: req.body.username,
-      id: newUserQuery.rows[0].id
-    }
-    res.json({ loggedIn: true, username: req.body.username })
-  } else {
-    res.json({ loggedIn: false, status: "Username taken" })
-  };
-
-});
+router
+  .post("/register", validateForm, attempRegister);
 
 module.exports = router;
