8010a09d4ed31b3345989014a7c9b276a2712d17
patriceandrea <andrea_pavico98@yahoo.ca>
2022-07-17 17:41:18 -0400

    adds the friendSchma function to the client side

--- server/controllers/validateForm.js
+++ server/controllers/validateForm.js
@@ -5,12 +5,6 @@ const formSchema = Yup.object({
   password: Yup.string().required("Password required").min(6, "Password too short").max(28, "Password too long")
 })
 
-const friendSchema = Yup.object({
-  friendName: Yup.string()
-    .required("Username required")
-    .min(6, "Invalid username")
-    .max(28, "Invalid Username ")
-})
 
 const validateForm = (req, res, next) => {
   const formData = req.body;
